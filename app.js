c65463869bd0577833c39e40db20781217367e7b
Murat <murat199611@mail.ru>
2020-12-16 00:51:57 +0300

    тест

--- app.js
+++ app.js
@@ -6,15 +6,9 @@ const cors = require("cors");
 const router = require("./routes");
 
 app.use(cors());
+app.options("*", cors());
 app.use(bodyParser.json());
 app.use("/", router);
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "origin, content-type, accept");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
 
 app.listen(PORT, (_) => {
   console.log(`listening to ${PORT}`);

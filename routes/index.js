e6428c0440c346a513bd5a2f867c743184abe9bb
Murat <murat199611@mail.ru>
2020-12-16 00:15:23 +0300

    новая попытка создать header

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,5 @@ const router = require("express").Router();
 const { response } = require("express");
 const { getPoems } = require("../controllers/poems");
 router.get("/", getPoems);
-response.setHeader("Access-Control-Allow-Origin", "*");
 
 module.exports = router;

46c8a7a130c68194f5343137eeb8be36b5306aed
Murat <murat199611@mail.ru>
2020-12-16 11:11:15 +0300

    тест

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.use(bodyParser.json());
 app.use("/", router);
 app.get("/*", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.end("<h1>Hello World</h1>");
   next();
 });
 

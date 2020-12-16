8f4697277b3ecfbf622f6b384892127db6c706db
Murat <murat199611@mail.ru>
2020-12-16 11:00:33 +0300

    тест

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const router = require("./routes");
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/", router);
+app.get("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 app.listen(PORT, (_) => {
   console.log(`listening to ${PORT}`);

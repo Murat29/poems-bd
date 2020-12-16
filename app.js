6e432f31f7b7227c2ed265b07a3d25889d03d779
Murat <murat199611@mail.ru>
2020-12-16 12:15:28 +0300

    тест

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const router = require("./routes");
 app.use(bodyParser.json());
 app.use("/", router);
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
   );

7eb64cd2174af34056f47ec2c59296ca17db965d
Murat <murat199611@mail.ru>
2020-12-17 15:29:25 +0300

    edit filter

--- controllers/poems.js
+++ controllers/poems.js
@@ -1,22 +1,26 @@
-const bd = require('../bd')
+const bd = require("../bd");
 
 const getPoems = (req, res) => {
-  const { q, onlyLines } = req.query
-  if (!q && !onlyLines) return res.send(bd)
+  const { q, onlyLines } = req.query;
+  if (!q && !onlyLines) return res.send(bd);
   const foundPoems = bd.reduce((sum, item) => {
-    const text = item.fields.text
-    const hasText = text.includes(q)
-    if (!hasText) return sum
+    const text = item.fields.text;
+    const hasText = text.includes(q);
+    if (!hasText) return sum;
     if (onlyLines) {
-      const lines = text.split(/\r?\n/).filter(line => line.includes(q))
-      return sum.concat({ ...item, fields: { ...item.fields, text: lines } })
+      const lines = text.split(/\r?\n/).find((line) => line.includes(q));
+      return sum.concat({ text: lines });
     }
-    return sum.concat(item)
-  }, [])
-  res.send(foundPoems)
-}
-
+    return sum.concat(item);
+  }, []);
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+  }
+  res.send(foundPoems[getRandomInt(0, foundPoems.length)]);
+};
 
 module.exports = {
-  getPoems
-}
+  getPoems,
+};
